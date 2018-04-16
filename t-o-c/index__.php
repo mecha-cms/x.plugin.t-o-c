477485860cff6ef0b8f6b4b8f3ee995dcb5d9c50
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-04-16 22:45:42 +0700

    Make `Block` Extension Optional

--- t-o-c/index__.php
+++ t-o-c/index__.php
@@ -1,43 +1,57 @@
 <?php
 
 function fn_toc($content, $lot = [], $that = null) {
-    global $site, $url;
-    // No header(s), skip anyway…
-    if (!$content || $site->is === 'pages' || stripos($content, '</h') === false) {
-        return $content;
-    }
-    // Disabled by the `toc` field from the global `$site->page` data, skip…
-    if (isset($site->page->toc) && !$site->page->toc) {
+    global $language, $site;
+    if (
+        // No content…
+        !$content ||
+        // Is error page…
+        $site->is('error') ||
+        // Is in page(s) view…
+        $site->is('pages') ||
+        // No header(s)…
+        stripos($content, '</h') === false
+    ) {
+        // Skip!
         return $content;
     }
     // Disabled by the `toc` field, skip…
-    if (isset($lot['toc']) && !$lot['toc'] || isset($that->toc) && !$that->toc) {
+    if ($that->toc !== null && !$that->toc) {
         return $content;
     }
-    // Add CSS file only if needed
-    Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'toc.min.css', 11);
-    Config::set('toc_id', Config::get('toc_id', 0) + 1);
-    global $language;
-    $state = Plugin::state(__DIR__);
+    // Add the CSS file only if needed
+    Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'toc.min.css');
+    Config::set('_toc_id', Config::get('_toc_id', 0) + 1);
+    $state = Plugin::state('t-o-c');
     $pattern = '#<h([1-6])(\s.*?)?>([\s\S]*?)<\/h\1>#i';
     $depth = $level = 0;
     $toc = "";
-    $state_block = Extend::state('block', 'union', [
-        1 => [['[[', ']]', '/']]
-    ]);
-    $o = $state_block[1][0][0];
-    $c = $state_block[1][0][1];
-    $d = $state_block[1][0][2];
-    $v = explode(',', trim(str_replace(',a,', "", ',' . HTML_WISE_I . ','), ','));
-    $type = $state['type'];
+    $toc_id = Config::get('_toc_id');
+    $toc_title = $language->toc;
     $id = $state['toc']['id'];
+    $type = $state['type'];
     $class = $state['toc']['class'];
     $class_x = $state['toc']['class_x'][$type];
-    $toc_id = Config::get('toc_id');
-    $dupe = [];
-    if ($type === 1 && strpos($content, $o . 'toc' . $c) === false && strpos($content, $o . 'toc' . $d . $c) === false) {
-        $content = $o . 'toc' . $c . "\n\n" . $content;
+    if ($block = Extend::exist('block')) {
+        $union = Extend::state('block', 'union');
+        $o = $union[1][0][0]; // `[[`
+        $c = $union[1][0][1]; // `]]`
+        $d = $union[1][0][2]; // `/`
+        $s = $union[1][1][3]; // ` `
+        if (
+            $type === 1 &&
+            // `[[toc]]`
+            strpos($content, $o . 'toc' . $c) === false &&
+            // `[[toc/]]`
+            strpos($content, $o . 'toc' . $d . $c) === false &&
+            // `[[toc `
+            strpos($content, $o . 'toc' . $s) === false
+        ) {
+            $content = Block::unite('toc', false, ['title' => $toc_title]) . "\n\n" . $content;
+        }
     }
+    $v = explode(',', trim(str_replace(',a,', "", ',' . HTML_WISE_I . ','), ','));
+    $dupe = [];
     if (preg_match_all($pattern, $content, $lot)) {
         for ($i = 0, $count = count($lot[0]); $i < $count; ++$i) {
             $level = (int) $lot[1][$i];
@@ -53,7 +67,7 @@ function fn_toc($content, $lot = [], $that = null) {
                 if (stripos($lot[0][$i], ' id="') !== false && preg_match('#\bid="(.*?)"#i', $lot[0][$i], $s)) {
                     $toc .= '<a href="#' . $s[1] . '">';
                 } else {
-                    $slug = h($title);
+                    $slug = To::slug($title);
                     // Append unique number to header ID if it is already exists
                     if (isset($dupe[$slug])) {
                         ++$dupe[$slug];
@@ -67,7 +81,7 @@ function fn_toc($content, $lot = [], $that = null) {
             }
         }
         $toc .= str_repeat('</li></ol>', $depth - ((int) $lot[1][0]) + 1);
-        $toc = '<div class="' . $class[0] . '" id="' . __replace__($id[0], ['id' => $toc_id]) . '"><div class="' . $class[0] . '-header"><h3>' . $language->toc . '</h3></div><div class="' . $class[0] . '-body">' . $toc . '</div></div>';
+        $toc = '<div class="' . $class[0] . '" id="' . __replace__($id[0], ['id' => $toc_id]) . '"><div class="' . $class[0] . '-header"><h3>' . X . '</h3></div><div class="' . $class[0] . '-body">' . $toc . '</div></div>';
         $i = 0;
         $dupe = [];
         $content = preg_replace_callback($pattern, function($lot) use($language, $type, $id, $class, $class_x, $toc_id, &$i, &$dupe) {
@@ -79,7 +93,7 @@ function fn_toc($content, $lot = [], $that = null) {
                     $attr = str_replace(' class="', ' class="' . $class[$type] . ' ', $lot[2]);
                 }
                 if (strpos($lot[2], ' id="') === false) {
-                    $attr .= ' id="' . __replace__($id[$type], ['id' => h($lot[3])]) . '"';
+                    $attr .= ' id="' . __replace__($id[$type], ['id' => To::slug($lot[3])]) . '"';
                 }
                 if ($type === 1) {
                     $mark = '<a class="' . $class[3] . '" href="#' . __replace__($id[3], ['id' => $toc_id . '-' . $i]) . '"></a>';
@@ -87,7 +101,7 @@ function fn_toc($content, $lot = [], $that = null) {
                     if (strpos($lot[2], ' id="') !== false && preg_match('#\bid="(.*?)"#i', $lot[2], $s)) {
                         $mark = '<a class="' . $class[3] . '" href="#' . $s[1] . '"></a>';
                     } else {
-                        $slug = h($lot[3]);
+                        $slug = To::slug($lot[3]);
                         // Append unique number to header ID if it is already exists
                         if (isset($dupe[$slug])) {
                             ++$dupe[$slug];
@@ -101,10 +115,10 @@ function fn_toc($content, $lot = [], $that = null) {
             }
             return $lot[0];
         }, $content);
-        return Block::replace('toc', $toc, $content);
+        return $block ? Block::replace('toc', function($content, $attr) use($toc, $toc_title) {
+            return str_replace(X, !empty($attr['title']) ? $attr['title'] : $toc_title, $toc);
+        }, $content) : str_replace(X, $toc_title, $toc) . $content;
     }
 }
 
 Hook::set('page.content', 'fn_toc', 10);
-
-// TODO: Make `block` extension optional!
