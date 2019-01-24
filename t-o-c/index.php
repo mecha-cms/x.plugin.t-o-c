fed9af4c0f5977dba2fdcb91b2cb6a649c172fa6
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-01-24 23:11:27 +0700

    Strict Naming

--- t-o-c/index.php
+++ t-o-c/index.php
@@ -1,38 +1,47 @@
 <?php namespace fn;
 
-function toc(string $content = "", array $lot = []) {
-    global $config, $language;
+function t_o_c($content) {
     $block = \Extend::exist('block');
+    $hash = X . $this->path . X;
     if (
         // No content…
         !$content ||
         // Is error page…
-        $config->is('error') ||
+        \Config::is('error') ||
         // Is in page(s) view…
-        $config->is('pages') ||
+        \Config::is('pages') ||
         // No header(s)…
         \stripos($content, '</h') === false
     ) {
         // Skip!
-        return $block ? \Block::replace('toc', "", $content) : $content;
+        return $block ? \Block::replace('t-o-c', "", $content) : $content;
     }
-    // Disabled by the `toc` field, skip…
-    if ($this->toc !== null && !$this->toc) {
+    // Disabled by the `state.t-o-c` field, skip…
+    $test = $this->get('state.t-o-c', $hash);
+    if ($test !== $hash && !$test) {
+        return $content;
+    }
+    $state = (array) \Plugin::state('t-o-c');
+    if ($test === true || $test === 1 || $test === 2) {
+        $test = ['type' => $test === true ? 1 : $test];
+    }
+    $state = \extend($state, (array) $test);
+    $type = $state['type'];
+    // Disabled by the `type` state, skip…
+    if ($type === false || $type === 0) {
         return $content;
     }
     // Add the CSS file only if needed
-    \Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'toc.min.css', 20.1);
-    \Config::set('_toc_id', \Config::get('_toc_id', 0) + 1);
-    $state = \Plugin::state('t-o-c');
+    \Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 't-o-c.min.css', 20.1);
+    \Config::set('_t-o-c', \Config::get('_t-o-c', 0) + 1);
     $pattern = '#<h([1-6])(\s[^<>]*?)?>([\s\S]*?)</h\1>#i';
     $depth = $level = 0;
-    $toc = "";
-    $toc_id = \Config::get('_toc_id');
-    $toc_title = $language->toc;
-    $id = $state['toc']['id'];
-    $type = $state['type'];
-    $class = $state['toc']['class'];
-    $class_x = $state['toc']['class/x'][$type];
+    $out = "";
+    $out_id = \Config::get('_t-o-c');
+    $out_title = \Language::get('t_o_c');
+    $id = $state['id'];
+    $class = $state['class'];
+    //$class_x = $state['class/x'][$type];
     if ($block) {
         $union = \Extend::state('block', 'union');
         $open = $union[1][0][0]; // `[[`
@@ -40,33 +49,33 @@ function toc(string $content = "", array $lot = []) {
         $end = $union[1][0][2]; // `/`
         $separator = $union[1][1][3]; // ` `
         if (
-            $type === 1 &&
-            // `[[toc]]`
-            \strpos($content, $open . 'toc' . $close) === false &&
-            // `[[toc/]]`
-            \strpos($content, $open . 'toc' . $end . $close) === false &&
-            // `[[toc `
-            \strpos($content, $open . 'toc' . $separator) === false
+            ($type === true || $type === 1) &&
+            // `[[t-o-c]]`
+            \strpos($content, $open . 't-o-c' . $close) === false &&
+            // `[[t-o-c/]]`
+            \strpos($content, $open . 't-o-c' . $end . $close) === false &&
+            // `[[t-o-c `
+            \strpos($content, $open . 't-o-c' . $separator) === false
         ) {
-            $content = \Block::unite('toc', false, ['title' => $toc_title]) . "\n\n" . $content;
+            $content = \Block::unite('t-o-c', false, ['title' => $out_title]) . "\n\n" . $content;
         }
     }
     $v = \explode(',', \trim(\str_replace(',a,', "", ',' . HTML_WISE_I . ','), ','));
     $dupe = [];
-    if (\preg_match_all($pattern, $content, $lot)) {
-        for ($i = 0, $count = \count($lot[0]); $i < $count; ++$i) {
-            $level = (int) $lot[1][$i];
-            if (\strpos($lot[0][$i], $class_x) === false) {
+    if (\preg_match_all($pattern, $content, $m)) {
+        for ($i = 0, $count = \count($m[0]); $i < $count; ++$i) {
+            $level = (int) $m[1][$i];
+            if (\strpos($m[0][$i], $class[4]) === false) {
                 if ($level > $depth) {
-                    $toc .= '<ol>';
+                    $out .= '<ol>';
                 } else {
-                    $toc .= \str_repeat('</li></ol>', $depth - $level);
-                    $toc .= '</li>';
+                    $out .= \str_repeat('</li></ol>', $depth - $level);
+                    $out .= '</li>';
                 }
-                $title = \w($lot[3][$i], $v);
-                $toc .= '<li id="' . \candy($id[3], ['id' => $toc_id . '-' . ($i + 1)]) . '">';
-                if (\stripos($lot[0][$i], ' id="') !== false && \preg_match('#\bid="(.*?)"#i', $lot[0][$i], $s)) {
-                    $toc .= '<a href="#' . $s[1] . '">';
+                $title = \w($m[3][$i], $v);
+                $out .= '<li id="' . \candy($id[3], ['id' => $out_id . '.' . ($i + 1)]) . '">';
+                if (\stripos($m[0][$i], ' id="') !== false && \preg_match('#\bid="(.*?)"#i', $m[0][$i], $s)) {
+                    $out .= '<a href="#' . $s[1] . '">';
                 } else {
                     $slug = \To::slug($title);
                     // Append unique number to header ID if it is already exists
@@ -75,51 +84,51 @@ function toc(string $content = "", array $lot = []) {
                     } else {
                         $dupe[$slug] = 0;
                     }
-                    $toc .= '<a href="#' . \candy($id[$type], ['id' => $slug . ($dupe[$slug] !== 0 ? '.' . $dupe[$slug] : "")]) . '">';
+                    $out .= '<a href="#' . \candy($id[$type], ['id' => $slug . ($dupe[$slug] !== 0 ? '.' . $dupe[$slug] : "")]) . '">';
                 }
-                $toc .= $title . '</a>&#x00A0;<span class="' . $class[3] . '"></span>';
+                $out .= $title . '</a>&#x00A0;<span class="' . $class[3] . '"></span>';
                 $depth = $level;
             }
         }
-        $toc .= \str_repeat('</li></ol>', $depth - ((int) $lot[1][0]) + 1);
-        $toc = '<div class="' . $class[0] . '" id="' . \candy($id[0], ['id' => $toc_id]) . '"><div class="' . $class[0] . '-header"><h3>' . X . '</h3></div><div class="' . $class[0] . '-body">' . $toc . '</div></div>';
+        $out .= \str_repeat('</li></ol>', $depth - ((int) $m[1][0]) + 1);
+        $out = '<div class="' . $class[0] . '" id="' . \candy($id[0], ['id' => $out_id]) . '"><div class="' . $class[0] . '-header"><h3>' . X . '</h3></div><div class="' . $class[0] . '-body">' . $out . '</div></div>';
         $i = 0;
         $dupe = [];
-        $content = \preg_replace_callback($pattern, function($lot) use($language, $type, $id, $class, $class_x, $toc_id, &$i, &$dupe) {
-            if (\strpos($lot[2], $class_x) === false) {
+        $content = \preg_replace_callback($pattern, function($m) use($type, $id, $class, $out_id, &$i, &$dupe) {
+            if (\strpos($m[2], $class[4]) === false) {
                 ++$i;
-                if (\strpos($lot[2], ' class="') === false) {
-                    $attr = ' class="' . $class[$type] . '"' . $lot[2];
+                $slug = \To::slug($m[3]);
+                // Append unique number to header ID if it is already exists
+                if (isset($dupe[$slug])) {
+                    ++$dupe[$slug];
                 } else {
-                    $attr = \str_replace(' class="', ' class="' . $class[$type] . ' ', $lot[2]);
+                    $dupe[$slug] = 0;
                 }
-                if (\strpos($lot[2], ' id="') === false) {
-                    $attr .= ' id="' . \candy($id[$type], ['id' => \To::slug($lot[3])]) . '"';
-                }
-                if ($type === 1) {
-                    $mark = '<a class="' . $class[3] . '" href="#' . \candy($id[3], ['id' => $toc_id . '-' . $i]) . '"></a>';
+                if ($type === true || $type === 1) {
+                    $mark = '<a class="' . $class[3] . '" href="#' . \candy($id[3], ['id' => $out_id . '.' . $i]) . '"></a>';
                 } else if ($type === 2) {
-                    if (\strpos($lot[2], ' id="') !== false && \preg_match('#\bid="(.*?)"#i', $lot[2], $s)) {
+                    if (\strpos($m[2], ' id="') !== false && \preg_match('#\bid="(.*?)"#i', $m[2], $s)) {
                         $mark = '<a class="' . $class[3] . '" href="#' . $s[1] . '"></a>';
                     } else {
-                        $slug = \To::slug($lot[3]);
-                        // Append unique number to header ID if it is already exists
-                        if (isset($dupe[$slug])) {
-                            ++$dupe[$slug];
-                        } else {
-                            $dupe[$slug] = 0;
-                        }
                         $mark = '<a class="' . $class[3] . '" href="#' . \candy($id[$type], ['id' => $slug . ($dupe[$slug] !== 0 ? '.' . $dupe[$slug] : "")]) . '"></a>';
                     }
                 }
-                return '<h' . $lot[1] . $attr . '>' . $lot[3] . '&#x00A0;' . $mark . '</h' . $lot[1] . '>';
+                if (\strpos($m[2], ' class="') === false) {
+                    $attr = ' class="' . $class[$type] . '"' . $m[2];
+                } else {
+                    $attr = \str_replace(' class="', ' class="' . $class[$type] . ' ', $m[2]);
+                }
+                if (\strpos($m[2], ' id="') === false) {
+                    $attr .= ' id="' . \candy($id[$type], ['id' => $slug . ($dupe[$slug] !== 0 ? '.' . $dupe[$slug] : "")]) . '"';
+                }
+                return '<h' . $m[1] . $attr . '>' . $m[3] . '&#x00A0;' . $mark . '</h' . $m[1] . '>';
             }
-            return $lot[0];
+            return $m[0];
         }, $content);
-        return $block ? \Block::replace('toc', function($content, $attr) use($toc, $toc_title) {
-            return \str_replace(X, $attr['title'] ?? $toc_title, $toc);
-        }, $content) : \str_replace(X, $toc_title, $toc) . $content;
+        return $block ? \Block::replace('t-o-c', function($content, $attr) use($out, $out_title) {
+            return \str_replace(X, $attr['title'] ?? $out_title, $out);
+        }, $content) : \str_replace(X, $out_title, $out) . $content;
     }
 }
 
-\Hook::set('page.content', __NAMESPACE__ . "\\toc", 10);
+\Hook::set('page.content', __NAMESPACE__ . "\\t_o_c", 10);
--- t-o-c/lot/state/config.php
+++ t-o-c/lot/state/config.php
@@ -1,13 +1,14 @@
 <?php
 
-// 1: Automatic TOC and section ID
-// 2: Automatic section ID only
+// `0`: Disable TOC
+// `1`: Automatic section ID and TOC
+// `2`: Automatic section ID only
+
+// `true`: Alias for `1`
+// `false`: Alias for `0`
 
 return [
     'type' => 1,
-    'toc' => [
-        'id' => ['toc:%{id}%', 'stage:%{id}%', 'point:%{id}%', 'a:%{id}%'],
-        'class' => ['toc', 'toc-stage', 'toc-point', 'toc-a'],
-        'class/x' => ["", 'not-toc-stage', 'not-toc-point']
-    ]
+    'id' => ['t-o-c:%{id}%', 'stage:%{id}%', 'point:%{id}%', 'a:%{id}%'],
+    'class' => ['t-o-c', 't-o-c:stage', 't-o-c:point', 't-o-c:a', 'x:t-o-c']
 ];
