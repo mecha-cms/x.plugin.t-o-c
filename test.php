e8822cca4768ddc354905e311bf7e3c42bae6517
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-03 21:35:53 +0700

    Update

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-$content = file_get_contents(__DIR__ . D . 'test');
+$content = file_get_contents(__DIR__ . D . 'test.txt');
 
 echo '<style>:target{background:#ff0}</style>';
 echo x\t_o_c\to\tree($content) . x\t_o_c\to\content($content);
